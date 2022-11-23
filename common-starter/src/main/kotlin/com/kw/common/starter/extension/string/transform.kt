beb2150d836f9c9b8d98b47c22f5e377d3d8730a
K W <kw@mail.com>
2022-11-23 09:25:53 +0700

    revise ext

--- common-starter/src/main/kotlin/com/kw/common/starter/extension/string/transform.kt
+++ common-starter/src/main/kotlin/com/kw/common/starter/extension/string/transform.kt
@@ -17,7 +17,7 @@ val camelRegex = "(?<=[a-zA-Z])[A-Z]".toRegex()
 fun String.toSnakeCase(): String = camelRegex.replace(this) { "_${it.value}" }.lowercase()
 
 /**
- * Parse string (especially json) into an object. otherwise, null
+ * Parse json string into an object. otherwise, null
  */
 inline fun <reified T : Any> String.parseOrNull(): T? {
     val logger: Logger = LoggerFactory.getLogger("StringExt")
@@ -41,21 +41,21 @@ inline fun <reified T : Any> String.parseOrNull(): T? {
 fun String.asResource() = {}::class.java.getResource(this)?.readText() ?: "read resource fail: $this"
 
 /**
- * Parse string (especially json) into a nullable object as body of ApiOutcome.Success
+ * Parse json string into a nullable object as body of ApiResponse.Success
  */
-inline fun <reified T : Any> String.asApiOutcomeSuccess(): ApiResponse.Success<T> =
+inline fun <reified T : Any> String.asApiResponseSuccess(): ApiResponse.Success<T> =
     this.parseOrNull<T>().let { ApiResponse.Success(httpStatus = HttpStatus.OK, httpHeaders = null, body = it) }
 
 /**
- * Parse string (especially json) into a nullable object as body of ApiOutcome.Failure
+ * Parse json string into a nullable object as body of ApiResponse.Failure
  */
-inline fun <reified T : Any> String.asApiOutcomeFailure(
+inline fun <reified T : Any> String.asApiResponseFailure(
     httpStatus: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
 ): ApiResponse.Failure<T> =
     this.parseOrNull<T>().let { ApiResponse.Failure(httpStatus = httpStatus, httpHeaders = null, body = it) }
 
 /**
- * Parse string (especially json) into a nullable object as bodyAsString of ApiOutcome.Error
+ * Parse json string into a nullable object as bodyAsString of ApiResponse.Error
  */
-fun String.asApiOutcomeError(httpStatus: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR): ApiResponse.Error =
+fun String.asApiResponseError(httpStatus: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR): ApiResponse.Error =
     ApiResponse.Error(httpStatus = httpStatus, httpHeaders = null, bodyAsString = this)
