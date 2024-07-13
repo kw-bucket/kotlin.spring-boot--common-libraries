50b39d557d7270770864eb5ff63a7cffb53c1b07
KW <79295752+kw-bucket@users.noreply.github.com>
2024-07-13 14:17:59 +0700

    Polish common mailing lib (#4)
    polish common mailing lib
    
    Co-authored-by: K W <kw@mail.com>

--- common-mailing/README.md
+++ common-mailing/README.md
@@ -1,3 +1,3 @@
-# COMMON-MAILABLE
+# COMMON-MAILING
 
 This artifact is an extension library of [common-stater](../common-starter/) which provide email notification trait
--- common-mailing/src/main/kotlin/com/kw/starter/mailing/service/api/NotificationApiService.kt
+++ common-mailing/src/main/kotlin/com/kw/starter/mailing/service/api/NotificationApiService.kt
@@ -1,11 +1,12 @@
-package com.kw.common.mail.service.api
+package com.kw.starter.mailing.service.api
 
-import com.kw.common.starter.constant.Constant
-import com.kw.common.starter.dto.AppResponse
-import com.kw.common.starter.service.api.ApiOutcome
-import com.kw.common.starter.service.api.ApiService
-import com.kw.common.mail.dto.notification.EmailNotificationRequest
-import com.kw.common.mail.dto.notification.asMap
+import com.kw.starter.common.dto.ApiOutput
+import com.kw.starter.common.http.constant.HeaderFields
+import com.kw.starter.common.log.constant.LogbackFields
+import com.kw.starter.common.service.api.ApiResponse
+import com.kw.starter.common.service.api.ApiService
+import com.kw.starter.mailing.dto.downstream.EmailRequest
+import com.kw.starter.mailing.dto.downstream.asMap
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.slf4j.MDC
@@ -21,21 +22,20 @@ import org.springframework.web.util.UriComponents
 import org.springframework.web.util.UriComponentsBuilder
 
 @Service
-class NotificationApiService(restTemplate: RestTemplate) : ApiService(restTemplate) {
-
+class NotificationApiService(restTemplate: RestTemplate) : ApiService(restTemplate = restTemplate) {
     private val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
-    @Value("\${api.notification.endpoints.send-email}")
+    @Value("\${api.mailing.endpoints.send-email}")
     private val sendEmailUrl: String = "send-email"
 
-    fun sendEmail(request: EmailNotificationRequest): ApiOutcome<AppResponse<Void>> {
+    fun sendEmail(request: EmailRequest): ApiResponse<ApiOutput<Nothing>> {
         val headers = HttpHeaders()
         headers.contentType = MediaType.MULTIPART_FORM_DATA
-        headers.set(Constant.Headers.CORRELATIONID, MDC.get(Constant.Context.CORRELATION_ID))
-        headers.set(Constant.Headers.X_CORRELATION_ID, MDC.get(Constant.Context.CORRELATION_ID))
+        headers.set(HeaderFields.CORRELATIONID, MDC.get(LogbackFields.CORRELATION_ID))
+        headers.set(HeaderFields.X_CORRELATION_ID, MDC.get(LogbackFields.CORRELATION_ID))
 
         val uriComponents: UriComponents = UriComponentsBuilder.fromHttpUrl(sendEmailUrl).build()
-        val responseType = object : ParameterizedTypeReference<AppResponse<Void>>() {}
+        val responseType = object : ParameterizedTypeReference<ApiOutput<Nothing>>() {}
         val arguments = request.asMap()
 
         logger.debug(
